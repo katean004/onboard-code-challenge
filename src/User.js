e55db579fb03fe52df23206e6ccd5a82d1b259a5
kate <kan004@ucr.edu>
2021-10-19 15:43:49 -0700

    edited readme and setup component layout

---
+++ src/User.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./User.css";
+
+class User extends Component {
+  render() {
+    return (
+      <div className="User">
+        <h1>I'm a User!</h1>
+      </div>
+    );
+  }
+}
+
+export default User;
