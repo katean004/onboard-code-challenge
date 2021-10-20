6d57f67b53f9916b10cb20e320004b1ef005d950
kate <kan004@ucr.edu>
2021-10-19 21:51:35 -0700

    added state and component mount

--- src/User.js
+++ src/User.js
@@ -7,94 +7,117 @@ import twitter from "./assets/twitter.svg";
 import poweredBy from "./assets/powered_by_onboard.svg";
 
 class User extends Component {
-  // Set default user data
-  static defaultProps = {
-    id: 5,
-    profile: {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userData: {},
+      loading: true
+    };
+  }
+
+  componentDidMount() {
+    const data = {
       id: 5,
-      linkedin_url: "https://www.linkedin.com/company/onboardio/",
-      twitter_url: "https://www.twitter.com/onboardio/",
-      instagram_url: "https://www.instagram.com/onboardio/",
-      website_url: "https://onboard.io",
-      bio: "This is my tagline.",
-      job_title: "Title"
-    },
-    avatar: "https://assets.onboard.io/assets/a/5/5/fswnuakkcn.jpg",
-    first_name: "John",
-    last_name: "Smith",
-    name: "John Smith",
-    created_at: "2020-10-01T19:40:40Z"
-  };
+      profile: {
+        id: 5,
+        linkedin_url: "https://www.linkedin.com/company/onboardio/",
+        twitter_url: "https://www.twitter.com/onboardio/",
+        instagram_url: "https://www.instagram.com/onboardio/",
+        website_url: "https://onboard.io",
+        bio: "This is my tagline.",
+        job_title: "Title"
+      },
+      avatar: "https://assets.onboard.io/assets/a/5/5/fswnuakkcn.jpg",
+      first_name: "John",
+      last_name: "Smith",
+      name: "John Smith",
+      created_at: "2020-10-01T19:40:40Z"
+    };
+
+    this.setState({ userData: data, loading: false });
+  }
 
   render() {
     return (
-      <div className="User">
-        {/* Onboard logo */}
-        <a
-          href={this.props.profile.website_url}
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img className="User-onboard-logo" src={logo} alt="onboard logo" />
-        </a>
-        {/* User info */}
-        <div className="User-info">
-          <img
-            className="User-img"
-            src={this.props.avatar}
-            alt="profile avatar"
-          />
-          <div className="User-text">
-            <h1 className="User-name">Hey, I'm {this.props.name}!</h1>
-            <h3 className="User-bio">{this.props.profile.bio}</h3>
+      <div>
+        {this.state.loading ? (
+          <h1>loading...</h1>
+        ) : (
+          <div className="User">
+            {/* Onboard logo */}
+            <a
+              href={this.state.userData.profile.website_url}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                className="User-onboard-logo"
+                src={logo}
+                alt="onboard logo"
+              />
+            </a>
+            {/* User info */}
+            <div className="User-info">
+              <img
+                className="User-img"
+                src={this.state.userData.avatar}
+                alt="profile avatar"
+              />
+              <div className="User-text">
+                <h1 className="User-name">
+                  Hey, I'm {this.state.userData.name}!
+                </h1>
+                <h3 className="User-bio">{this.state.userData.profile.bio}</h3>
+              </div>
+            </div>
+            {/* User social media */}
+            <div className="User-socials">
+              <a
+                href={this.state.userData.profile.twitter_url}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img
+                  className="User-social-icon"
+                  src={twitter}
+                  alt="twitter icon"
+                />
+              </a>
+              <a
+                href={this.state.userData.profile.instagram_url}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img
+                  className="User-social-icon"
+                  src={instagram}
+                  alt="instagram icon"
+                />
+              </a>
+              <a
+                href={this.state.userData.profile.linkedin_url}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img
+                  className="User-social-icon"
+                  src={linkedin}
+                  alt="linkedin icon"
+                />
+              </a>
+            </div>
+            {/* Footer and logo */}
+            <footer className="User-footer">
+              <a
+                href={this.state.userData.profile.website_url}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src={poweredBy} alt="onboard logo" />
+              </a>
+            </footer>
           </div>
-        </div>
-        {/* User social media */}
-        <div className="User-socials">
-          <a
-            href={this.props.profile.twitter_url}
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img
-              className="User-social-icon"
-              src={twitter}
-              alt="twitter icon"
-            />
-          </a>
-          <a
-            href={this.props.profile.instagram_url}
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img
-              className="User-social-icon"
-              src={instagram}
-              alt="instagram icon"
-            />
-          </a>
-          <a
-            href={this.props.profile.linkedin_url}
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img
-              className="User-social-icon"
-              src={linkedin}
-              alt="linkedin icon"
-            />
-          </a>
-        </div>
-        {/* Footer and logo */}
-        <footer className="User-footer">
-          <a
-            href={this.props.profile.website_url}
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img src={poweredBy} alt="onboard logo" />
-          </a>
-        </footer>
+        )}
       </div>
     );
   }
